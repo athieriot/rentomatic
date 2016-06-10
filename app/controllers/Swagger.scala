3baaaa872d187d9d64c03b9e32f31c85057d416c
Aurélien Thieriot <a.thieriot@gmail.com>
2016-06-11 00:13:48 +0100

    Fix Swagger on Heroku
    - Heroku is using Https and provide (obviously) another domain name than
      localhost. And Swagger needs both scheme and host to be provided in
      the configuration.

--- app/controllers/Swagger.scala
+++ app/controllers/Swagger.scala
@@ -3,20 +3,26 @@ package controllers
 import javax.inject.Inject
 
 import com.iheart.playSwagger.SwaggerSpecGenerator
+import play.api.Configuration
 import play.api.cache.Cached
 import play.api.libs.concurrent.Execution.Implicits._
+import play.api.libs.json.JsString
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.Future
 
-class Swagger @Inject()(cached: Cached) extends Controller {
+class Swagger @Inject()(cached: Cached,
+                        config: Configuration) extends Controller {
   implicit val cl = getClass.getClassLoader
 
   private lazy val generator = SwaggerSpecGenerator()
 
   def specs = cached("swaggerDef") {
     Action.async { _ =>
-      Future.fromTry(generator.generate()).map(Ok(_))
+      val hostname = config.getString("play.swagger.host").getOrElse("localhost:9000")
+      val scheme = config.getString("play.swagger.scheme").getOrElse("http")
+
+      Future.fromTry(generator.generate().map(_ + ("host" -> JsString(hostname)) + ("scheme" -> JsString(scheme)))).map(Ok(_))
     }
   }
 }
